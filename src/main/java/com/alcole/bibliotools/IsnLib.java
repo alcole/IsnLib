2bf83442850a3428849ea16c401f76dfbccc311b
alex-cole <alexjcole01@gmail.com>
2019-03-15 13:35:54 +0000

    style refactor validateISN -> isValidIsn

--- src/main/java/com/alcole/bibliotools/IsnLib.java
+++ src/main/java/com/alcole/bibliotools/IsnLib.java
@@ -1,242 +1,263 @@
 /**
  * MIT License
  *
- * Copyright (c) 2016 Alexander Cole
+ * <p>Copyright (c) 2016 Alexander Cole
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
+ * <p>Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ * <p>The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * <p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-
 package com.alcole.bibliotools;
 
 import java.util.regex.Pattern;
 
 /**
- * This class consists of static methods that can be used when working with
- * bibliographic identifiers, that is the International Standard Numbers for
- * Books(ISBN), Serials(ISSN) and written music(ISMN).
- * 'isn' is used throughout to mean any of ISBN, ISSN, ISMN
- *
+ * This class consists of static methods that can be used when working with bibliographic
+ * identifiers, that is the International Standard Numbers for Books(ISBN), Serials(ISSN) and
+ * written music(ISMN). 'isn' is used throughout to mean any of ISBN, ISSN, ISMN
  *
  * @author Alex Cole
- *
  */
-public class IsnLib {
+public final class IsnLib {
 
-    private static final int ISSN_LENGTH = 8;
-    private static final int ISBN10_LENGTH = 10;
-    private static final int ISBN13_LENGTH = 13;
+  private static final int ISSN_LENGTH = 8;
+  private static final int ISBN10_LENGTH = 10;
+  private static final int ISBN13_LENGTH = 13;
 
-    private static final String ISSN_PATTERN = "\\d{4}[-]?\\d{3}[0-9xX]{1}";
-    private static final String ISBN10_PATTERN = "[\\d|\\-]{9,13}[0-9xX]{1}";
-    private static final String EAN13_PATTERN = "[9]{1}[7]{1}[7|8|9][\\d|\\-]{9,13}[\\d]{1}";
+  private static final String ISSN_PATTERN = "\\d{4}[-]?\\d{3}[0-9xX]{1}";
+  private static final String ISBN10_PATTERN = "[\\d|\\-]{9,13}[0-9xX]{1}";
+  private static final String EAN13_PATTERN = "[9]{1}[7]{1}[7|8|9][\\d|\\-]{9,13}[\\d]{1}";
 
-    private static final String ISSNEAN13_PREFIX = "977";
-    private static final String ISMN_PREFIX = "9790";
-    private static final String ISBN_PREFIX1 = "978";
-    private static final String ISBN_PREFIX2 = "979";
+  private static final String ISSNEAN13_PREFIX = "977";
+  private static final String ISMN_PREFIX = "9790";
+  private static final String ISBN_PREFIX1 = "978";
+  private static final String ISBN_PREFIX2 = "979";
 
-    public enum IdentifierType {
-        ISSN, ISBN10, ISBN13, ISMN, ISSNEAN13, OTHER
+  private IsnLib() {
+    // not called
+  }
+
+  /**
+   * Strips the hyphens.
+   *
+   * @param isn The ISN to format
+   * @return the string with hyphens removed
+   */
+  public static String canonicalForm(final String isn) {
+    return isn.replace("-", "").trim();
+  }
+
+  /**
+   * pads with leading zeroes to desired length works only with ISSN and ISBN-10.
+   *
+   * @param isn the ISN to pad
+   * @param length the target length
+   * @return the isn string padded with zeroes
+   * @throws IllegalArgumentException if provided length is not a valid isn length
+   */
+  public static String pad(final String isn, final int length) throws IllegalArgumentException {
+    if (!(length == ISSN_LENGTH || length == ISBN10_LENGTH)) {
+      throw new IllegalArgumentException();
+    }
+    String padded = isn;
+    while (padded.length() < length) {
+      padded = "0" + padded;
+    }
+    return padded;
+  }
+
+  /**
+   * takes a String and checks if the check digit matches calculated check digit.
+   *
+   * @param isn the ISN to validate
+   * @return true for isn strings with supplied with valid check digit, false otherwise
+   */
+  public static boolean isValidIsn(final String isn) {
+    if (!(Pattern.matches(ISSN_PATTERN, isn)
+        || Pattern.matches(ISBN10_PATTERN, isn)
+        || Pattern.matches(EAN13_PATTERN, isn))) {
+      return false;
     }
 
-    /**
-     * Strips the hyphens
-     * @param  isn
-     * @return the string with hyphens removed
-     */
-    public static String canonicalForm(String isn) {
-        return isn.replace("-", "").trim();
+    String canon = canonicalForm(isn);
+
+    if (canon.length() == ISBN13_LENGTH) {
+      return generateCheck(canon) == canon.charAt(12);
+    } else if (canon.length() == ISBN10_LENGTH) {
+      return validate(canon, ISBN10_LENGTH);
+    } else if (canon.length() == ISSN_LENGTH) {
+      return validate(canon, ISSN_LENGTH);
+    } else {
+      return false;
+    }
+  }
+
+  /**
+   * helper method for validateIsn uses fast accumulator algorithm for ISBN-10 and ISSN.
+   *
+   * @param isn isn to validate
+   * @param length length of isn
+   * @return true for isns supplied with valid check digit, false otherwise
+   */
+  private static boolean validate(final String isn, final int length) {
+    if (!(length == ISSN_LENGTH || length == ISBN10_LENGTH)) {
+      throw new IllegalArgumentException();
     }
 
-    /**
-     * pads with leading zeroes to desired length
-     * works only with ISSN and ISBN-10
-     * @param  isn
-     * @param  length
-     * @return the isn string padded with zeroes
-     * @throws IllegalArgumentException if provided length is not a valid isn length
-     */
-    public static String pad(String isn, int length) throws IllegalArgumentException {
-        if (!(length == ISSN_LENGTH || length == ISBN10_LENGTH))
-            throw new IllegalArgumentException();
-
-        while (isn.length() < length) {
-            isn = "0" + isn;
-        }
-        return isn;
+    int s = 0, t = 0;
+    for (int i = 0; i < length - 1; i++) {
+      t += Character.getNumericValue(isn.charAt(i));
+      s += t;
     }
-
-    /**
-     * takes a String and checks if the check digit matches calculated check digit
-     *
-     * @param  isn
-     * @return true for isn strings with supplied with valid check digit, false otherwise
-     */
-    public static boolean validateIsn(String isn) {
-        if (!(Pattern.matches(ISSN_PATTERN, isn) ||
-                Pattern.matches(ISBN10_PATTERN, isn) ||
-                Pattern.matches(EAN13_PATTERN, isn))) {
-            return false;
-        }
-
-        String canon = canonicalForm(isn);
-
-        if (canon.length() == ISBN13_LENGTH) {
-            return generateCheck(canon) == canon.charAt(12);
-        } else if (canon.length() == ISBN10_LENGTH) {
-            return validate(canon, ISBN10_LENGTH);
-        } else if (canon.length() == ISSN_LENGTH) {
-            return validate(canon, ISSN_LENGTH);
-        } else {
-            return false;
-        }
+    if (isn.charAt(length - 1) == 'X' || isn.charAt(length - 1) == 'x') {
+      t += 10;
+    } else {
+      t += Character.getNumericValue(isn.charAt(length - 1));
     }
+    s += t;
+    return (s % 11) == 0;
+  }
 
-    /**
-     * helper method for validateIsn uses fast accumulator algorithm for ISBN-10 and ISSN
-     *
-     * @param  isn
-     * @param  length
-     * @return true for isns supplied with valid check digit, false otherwise
-     */
-    private static boolean validate(String isn, int length) {
-        if (!(length == ISSN_LENGTH || length == ISBN10_LENGTH))
-            throw new IllegalArgumentException();
+  /**
+   * @param isn the isn with or without the supplied check digit
+   * @return the char of the check digit 0-9 or 'X'
+   * @throws IllegalArgumentException for strings of the wrong length
+   */
+  public static char generateCheck(final String isn) throws IllegalArgumentException {
+    if (!(isn.length() == ISBN13_LENGTH
+        || isn.length() == ISBN10_LENGTH
+        || isn.length() == ISBN13_LENGTH - 1
+        || isn.length() == ISBN10_LENGTH - 1
+        || isn.length() == ISSN_LENGTH
+        || isn.length() == ISSN_LENGTH - 1)) throw new IllegalArgumentException();
 
-        int s = 0, t = 0;
-        for (int i = 0; i < length - 1; i++) {
-            t += Character.getNumericValue(isn.charAt(i));
-            s += t;
-        }
-        if (isn.charAt(length - 1) == 'X' || isn.charAt(length - 1) == 'x') {
-            t += 10;
-        } else {
-            t += Character.getNumericValue(isn.charAt(length - 1));
-        }
-        s += t;
-        return (s % 11) == 0;
+    int checkSum = 0;
+    if (isn.length() == ISBN13_LENGTH || isn.length() == ISBN13_LENGTH - 1) {
+      for (int i = 1; i < 12; i = i + 2) {
+        checkSum += Character.getNumericValue(isn.charAt(i));
+      }
+      checkSum *= 3;
+      for (int i = 0; i < 12; i = i + 2) {
+        checkSum += Character.getNumericValue(isn.charAt(i));
+      }
+      checkSum = (10 - (checkSum % 10));
+      if (checkSum > 9) {
+        checkSum = 0;
+      } else {
+        checkSum = checkSum;
+      }
+      return (char) (checkSum + 48);
+    } else {
+      return generateCheckIsbn10Issn(isn, ((isn.length() + 1) / 2) * 2);
     }
+  }
 
-    /**
-     *
-     * @param  isn the isn with or without the supplied check digit
-     * @return the char of the check digit 0-9 or 'X'
-     * @throws IllegalArgumentException for strings of the wrong length
-     */
-    public static char generateCheck(String isn) throws IllegalArgumentException {
-        if (!(isn.length() == ISBN13_LENGTH || isn.length() == ISBN10_LENGTH ||
-                isn.length() == ISBN13_LENGTH - 1 || isn.length() == ISBN10_LENGTH - 1 ||
-                isn.length() == ISSN_LENGTH || isn.length() == ISSN_LENGTH - 1))
-            throw new IllegalArgumentException();
-
-        int checkSum = 0;
-        if (isn.length() == ISBN13_LENGTH || isn.length() == ISBN13_LENGTH - 1) {
-            for (int i = 1; i < 12; i = i + 2) {
-                checkSum += Character.getNumericValue(isn.charAt(i));
-            }
-            checkSum *= 3;
-            for (int i = 0; i < 12; i = i + 2) {
-                checkSum += Character.getNumericValue(isn.charAt(i));
-            }
-            checkSum = (10 - (checkSum % 10));
-            checkSum = checkSum > 9 ? 0 : checkSum;
-            return (char) (checkSum + 48);
-        } else {
-            return generateCheckIsbn10Issn(isn, ((isn.length() + 1) / 2) * 2);
-        }
+  /**
+   * @param isn the isn to get check digit for
+   * @param length - the target length for the supplied isn
+   * @return the character corresponding to the check digit (0-9 or 'X')
+   */
+  private static char generateCheckIsbn10Issn(final String isn, final int length) {
+    int checkSum = 0;
+    for (int i = 0; i < length - 1; i++) {
+      checkSum += Character.getNumericValue(isn.charAt(i)) * (length - i);
     }
-
-    /**
-     *
-     * @param  isn
-     * @param  length - the target length for the supplied isn
-     * @return the character corresponding to the check digit (0-9 or 'X')
-     */
-    private static char generateCheckIsbn10Issn(String isn, int length) {
-        int checkSum = 0;
-        for (int i = 0; i < length - 1; i++) {
-            checkSum += Character.getNumericValue(isn.charAt(i)) * (length - i);
-        }
-        checkSum = (11 - checkSum % 11) % 11;
-        return checkSum == 10 ? 'X' : (char) (checkSum + 48);
+    checkSum = (11 - checkSum % 11) % 11;
+    if (checkSum == 10) {
+      return 'X';
+    } else {
+      return (char) (checkSum + 48);
     }
+  }
 
-    /**
-     *
-     * @param  isbn10
-     * @return the ISBN-13 representation of the supplied ISBN-10
-     */
-    public static String isbn10To13(String isbn10) {
-        return isbn10To13(ISBN_PREFIX1, isbn10);
+  /**
+   * @param isbn10 the ISBN to convert
+   * @return the ISBN-13 representation of the supplied ISBN-10
+   */
+  public static String isbn10To13(final String isbn10) {
+    return isbn10To13(ISBN_PREFIX1, isbn10);
+  }
+
+  public static String isbn10To13(final String prefix, final String isbn10) {
+    return prefix + isbn10.substring(0, 9) + generateCheck(prefix + isbn10);
+  }
+
+  /**
+   * verifies if the given isn string is valid if padded with zeroes to make the correct length.
+   *
+   * @param isn isn to check
+   * @param isnType type of the issn
+   * @return true if padded isn validates by check sum digit
+   */
+  public static boolean lostLeadingZeroes(final String isn, final IdentifierType isnType) {
+    switch (isnType) {
+      case ISSN:
+        return validate(pad(isn, ISSN_LENGTH), ISSN_LENGTH);
+      case ISBN10:
+        return validate(pad(isn, ISBN10_LENGTH), ISBN10_LENGTH);
+      default:
+        return false;
     }
+  }
 
-    public static String isbn10To13(String prefix, String isbn10) {
-        return prefix + isbn10.substring(0, 9) + generateCheck(prefix + isbn10);
+  /**
+   * takes a 13 digit EAN and returns the ISSN if available.
+   *
+   * @param ean 13 digit EAN code
+   * @return String ISSN if EAN contains, else returns original EAN code
+   */
+  public static String issnFromEan13(final String ean) {
+    if (!(Pattern.matches(EAN13_PATTERN, ean) || ean.substring(0, 3).equals(ISSNEAN13_PREFIX))) {
+      return ean;
     }
+    return canonicalForm(ean).substring(3, 10) + generateCheck(canonicalForm(ean).substring(3, 10));
+  }
 
-    /**
-     * verifies if the given isn string is valid if padded with zeroes to make the correct length
-     *
-     * @param  isn
-     * @param  isnType
-     * @return true if padded isn validates by check sum digit
-     */
-    public static boolean lostLeadingZeroes(String isn, IdentifierType isnType) {
-        switch (isnType) {
-            case ISSN:
-                return validate(pad(isn, ISSN_LENGTH), ISSN_LENGTH);
-            case ISBN10:
-                return validate(pad(isn, ISBN10_LENGTH), ISBN10_LENGTH);
-            default:
-                return false;
-        }
+  /**
+   * @param isn isn to get the type
+   * @return IdentifierType from Enum IdentifierType
+   * @throws IllegalArgumentException for strings of a length that cannot be an isn and if the check
+   *     digit is not valid
+   */
+  public static IdentifierType getType(final String isn) throws IllegalArgumentException {
+    if (!isValidIsn(isn)) {
+      throw new IllegalArgumentException("invalid identifier");
     }
+    int length = canonicalForm(isn).length();
 
-    /**
-     * takes a 13 digit EAN and returns the ISSN if available
-     *
-     * @param  ean 13 digit EAN code
-     * @return String ISSN if EAN contains, else returns original EAN code
-     */
-    public static String issnFromEan13(String ean) {
-        if (!(Pattern.matches(EAN13_PATTERN, ean) || ean.substring(0, 3).equals(ISSNEAN13_PREFIX))) return ean;
-        return canonicalForm(ean).substring(3, 10) + generateCheck(canonicalForm(ean).substring(3, 10));
+    if (length == 8) {
+      return IdentifierType.ISSN;
+    } else if (length == 10) {
+      return IdentifierType.ISBN10;
+    } else if (isn.substring(0, 3).equals(ISSNEAN13_PREFIX)) {
+      return IdentifierType.ISSNEAN13;
+    } else if (isn.substring(0, 4).equals(ISMN_PREFIX)) {
+      return IdentifierType.ISMN;
+    } else if (isn.substring(0, 3).equals(ISBN_PREFIX1)
+        || isn.substring(0, 3).equals(ISBN_PREFIX2)) {
+      return IdentifierType.ISBN13;
+    } else {
+      return IdentifierType.OTHER;
     }
+  }
 
-    /**
-     *
-     * @param  isn
-     * @return IdentifierType from Enum IdentifierType
-     * @throws IllegalArgumentException for strings of a length that cannot be an isn and
-     * if the check digit is not valid
-     */
-    public static IdentifierType getType(String isn) throws IllegalArgumentException {
-        if (!validateIsn(isn)) throw new IllegalArgumentException("invalid identifier");
-        isn = canonicalForm(isn);
-        int length = isn.length();
-
-        if (length == 8) return IdentifierType.ISSN;
-        else if (length == 10) return IdentifierType.ISBN10;
-        else if (isn.substring(0, 3).equals(ISSNEAN13_PREFIX)) return IdentifierType.ISSNEAN13;
-        else if (isn.substring(0, 4).equals(ISMN_PREFIX)) return IdentifierType.ISMN;
-        else if (isn.substring(0, 3).equals(ISBN_PREFIX1) || isn.substring(0, 3).equals(ISBN_PREFIX2))
-            return IdentifierType.ISBN13;
-        else return IdentifierType.OTHER;
-    }
+  public enum IdentifierType {
+    ISSN,
+    ISBN10,
+    ISBN13,
+    ISMN,
+    ISSNEAN13,
+    OTHER
+  }
 }
--- src/test/java/com/alcole/bibliotools/IsnLibTest.java
+++ src/test/java/com/alcole/bibliotools/IsnLibTest.java
@@ -1,87 +1,83 @@
 package com.alcole.bibliotools;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 import static com.alcole.bibliotools.IsnLib.*;
+import static org.junit.Assert.*;
 
-/**
- * Created by alex on 05/11/16.
- */
+/** Created by alex on 05/11/16. */
 public class IsnLibTest {
 
-    @Test
-    public void testCanonicalForm() {
-        assertEquals("978140885565", canonicalForm("978140885565 "));
-        assertEquals("9783161484100", canonicalForm("978-3-16-148410-0"));
-        assertEquals("978140885565", canonicalForm("978140885565"));
-    }
+  @Test
+  public void testCanonicalForm() {
+    assertEquals("978140885565", canonicalForm("978140885565 "));
+    assertEquals("9783161484100", canonicalForm("978-3-16-148410-0"));
+    assertEquals("978140885565", canonicalForm("978140885565"));
+  }
 
-    @Test
-    public void testPadLength() {
-        assertEquals(8, pad("14664", 8).length());
-        assertEquals(10, pad("14664", 10).length());
-    }
+  @Test
+  public void testPadLength() {
+    assertEquals(8, pad("14664", 8).length());
+    assertEquals(10, pad("14664", 10).length());
+  }
 
-    @Test
-    public void testPadResult() {
-        assertEquals("00014664", pad("14664", 8));
-        assertEquals("0000014664", pad("14664", 10));
-    }
+  @Test
+  public void testPadResult() {
+    assertEquals("00014664", pad("14664", 8));
+    assertEquals("0000014664", pad("14664", 10));
+  }
 
-    @Test
-    public void testValidateIsn() {
-        assertTrue(validateIsn("9781408855652"));
-        assertTrue(validateIsn("00014664"));
-        assertTrue(validateIsn("978-0805071665"));
-        assertTrue(validateIsn("0805071660"));
-        assertTrue(validateIsn("9772434561006"));
+  @Test
+  public void testIsValidIsn() {
+    assertTrue(isValidIsn("9781408855652"));
+    assertTrue(isValidIsn("00014664"));
+    assertTrue(isValidIsn("978-0805071665"));
+    assertTrue(isValidIsn("0805071660"));
+    assertTrue(isValidIsn("9772434561006"));
 
-        assertFalse(validateIsn("0805071661"));
-        assertFalse("invalid form", validateIsn("0-8-0-5-0-7-1-6-6-1"));
+    assertFalse(isValidIsn("0805071661"));
+    assertFalse("invalid form", isValidIsn("0-8-0-5-0-7-1-6-6-1"));
 
-        assertTrue(validateIsn("2434561X"));
-        assertTrue(validateIsn("2434561x"));
-    }
+    assertTrue(isValidIsn("2434561X"));
+    assertTrue(isValidIsn("2434561x"));
+  }
 
-    @Test
-    public void testGenerateCheck() {
-        assertEquals("valid check digit", '2', generateCheck("978140885565"));
-        assertEquals('X', generateCheck("2434561X"));
-        assertEquals('X', generateCheck("2434561"));
-        assertEquals('1', generateCheck("184854957"));
-    }
+  @Test
+  public void testGenerateCheck() {
+    assertEquals("valid check digit", '2', generateCheck("978140885565"));
+    assertEquals('X', generateCheck("2434561X"));
+    assertEquals('X', generateCheck("2434561"));
+    assertEquals('1', generateCheck("184854957"));
+  }
 
-    @Test
-    public void testIsbn10To13String() {
-        assertEquals("9780262510875", isbn10To13("0262510871"));
-    }
+  @Test
+  public void testIsbn10To13String() {
+    assertEquals("9780262510875", isbn10To13("0262510871"));
+  }
 
-    @Test
-    public void testIsbn10To13StringString() {
-        assertEquals("9780262510875", isbn10To13("978", "0262510871"));
-    }
+  @Test
+  public void testIsbn10To13StringString() {
+    assertEquals("9780262510875", isbn10To13("978", "0262510871"));
+  }
 
-    @Test
-    public void testLostLeadingZeroes() {
-        assertTrue(lostLeadingZeroes("306406152", IdentifierType.ISBN10));
-        assertTrue(lostLeadingZeroes("14664", IdentifierType.ISSN));
-        assertFalse(lostLeadingZeroes("4664", IdentifierType.ISSN));
-    }
+  @Test
+  public void testLostLeadingZeroes() {
+    assertTrue(lostLeadingZeroes("306406152", IdentifierType.ISBN10));
+    assertTrue(lostLeadingZeroes("14664", IdentifierType.ISSN));
+    assertFalse(lostLeadingZeroes("4664", IdentifierType.ISSN));
+  }
 
-    @Test
-    public void testIssnFromEan13() {
-        assertEquals("2434561X", issnFromEan13("9772434561006"));
-    }
+  @Test
+  public void testIssnFromEan13() {
+    assertEquals("2434561X", issnFromEan13("9772434561006"));
+  }
 
-    @Test
-    public void testGetType() {
-        assertEquals(IdentifierType.ISSN, getType("00014664"));
-        assertEquals(IdentifierType.ISBN13, getType("9780262510875"));
-        assertEquals(IdentifierType.ISMN, getType("9790260000438"));
-        assertEquals(IdentifierType.ISSNEAN13, getType("9772434561006"));
-        assertEquals(IdentifierType.ISSNEAN13.toString(), getType("9772434561006").toString());
-    }
-
+  @Test
+  public void testGetType() {
+    assertEquals(IdentifierType.ISSN, getType("00014664"));
+    assertEquals(IdentifierType.ISBN13, getType("9780262510875"));
+    assertEquals(IdentifierType.ISMN, getType("9790260000438"));
+    assertEquals(IdentifierType.ISSNEAN13, getType("9772434561006"));
+    assertEquals(IdentifierType.ISSNEAN13.toString(), getType("9772434561006").toString());
+  }
 }
